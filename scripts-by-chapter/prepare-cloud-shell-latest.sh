ce5cf3ebaf671e90147f68230639345d49f69b26
Huy Le <62027877+giahuy102@users.noreply.github.com>
2026-04-28 17:16:57 +0700

    Update prepare-cloud-shell-latest.sh

--- scripts-by-chapter/prepare-cloud-shell-latest.sh
+++ scripts-by-chapter/prepare-cloud-shell-latest.sh
@@ -1,3 +1,7 @@
+echo "---------- INSTALLING NANO ----------"
+sudo yum install nano -y
+
+echo "---------- INSTALLING EKSCTL ----------"
 curl --location "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" -o eksctl.tar.gz
 tar -xzf eksctl.tar.gz
 sudo mv eksctl /usr/local/bin
