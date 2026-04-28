cc1271e7a754d46b6c791bdf82d9ae6bf7994fe8
Huy Le <62027877+giahuy102@users.noreply.github.com>
2026-04-28 17:17:13 +0700

    Update prepare-cloud-shell-latest.sh

--- scripts-by-chapter/prepare-cloud-shell-latest.sh
+++ scripts-by-chapter/prepare-cloud-shell-latest.sh
@@ -5,3 +5,7 @@ echo "---------- INSTALLING EKSCTL ----------"
 curl --location "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" -o eksctl.tar.gz
 tar -xzf eksctl.tar.gz
 sudo mv eksctl /usr/local/bin
+
+echo "---------- INSTALLING HELM ----------"
+export VERIFY_CHECKSUM=false
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
